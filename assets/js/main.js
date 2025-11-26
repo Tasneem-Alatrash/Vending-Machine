d850774004613835043f653a166f32e9fdabd2ef
Tasneem-Alatrash <tasneemalatrash4@gmail.com>
2025-11-26 22:41:50 +0200

    DFA Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const coins10 = document.querySelector(".coins10");
 const Reset = document.querySelector(".Reset");
 const price = 25;
 let total = 0;
+let state = "q0";
+let history = ["q0"];
 let name = "None";
 
 coins5.addEventListener("click", () => insertcoins(5));
@@ -14,15 +16,31 @@ images.forEach((img) => {
   img.addEventListener("click", (e) => {
     name = e.target.alt;
     document.querySelector(".selected-product").textContent = name;
+    updateDFA(`Product selected ${name}, Now insert coins.`, "alert-info");
   });
 });
 
 const insertcoins = (coins) => {
   if (name === "None") {
-    console.log("please enter product");
+    updateDFA("Pleace select a product first ! ", "alert-warning");
     return;
   }
   total += coins;
+  recalcState();
+  history.push(state);
+  if (total < price) {
+    updateDFA(
+      `Product : ${name}. Not enough. You still need ${price - total} coins `,
+      "alert-info"
+    );
+  } else if (total === price) {
+    updateDFA(`Product : ${name}. item dispensed.`, "alert-success");
+  } else {
+    updateDFA(
+      `Product : ${name}. item dispensed. change returend ${total - price}`,
+      "alert-success"
+    );
+  }
   UpdateProgressBar();
 };
 
@@ -36,6 +54,39 @@ const UpdateProgressBar = () => {
 const ResetMachine = () => {
   total = 0;
   name = "None";
+  history = ["q0"];
+  state = "q0";
+
   document.querySelector(".selected-product").textContent = name;
+  updateDFA("Waiting for coins...", "alert-secondry");
   UpdateProgressBar();
 };
+
+const updateDFA = (Message, alertClass) => {
+  document.querySelector(".AState").textContent = total;
+  document.querySelector(".CState").textContent = state;
+
+  document.querySelector(".message").textContent = Message;
+  document
+    .querySelector(".message")
+    .classList.remove("alert-success", "alert-warning", "alert-info");
+  document.querySelector(".message").classList.add(`${alertClass}`);
+
+  document.querySelector(".state-history").textContent = history.join(" → ");
+};
+const recalcState = () => {
+  if (total >= price) {
+    state = "qpaid";
+  } else if (total === 0) {
+    state = "q0";
+  } else if (total === 5) {
+    state = "q5";
+  } else if (total === 10) {
+    state = "q10";
+  } else if (total === 15) {
+    state = "q15";
+  } else if (total === 20) {
+    state = "q20";
+  }
+};
+updateDFA("Waiting for coins...", "alert-secondry");
